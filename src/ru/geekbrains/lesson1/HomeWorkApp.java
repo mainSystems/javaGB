284d904debdbac1b982052be02a49e6c1acf13ba
DnMing <neverfaten@gmail.com>
2022-02-26 23:20:58 +0300

    First home work

---
+++ src/ru/geekbrains/lesson1/HomeWorkApp.java
@@ -0,0 +1,47 @@
+package ru.geekbrains.lesson1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void  printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = 8;
+        int b = -16;
+
+        if(a + b >= 0){
+            System.out.println("Сумма положительная");
+        } else{
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 8;
+
+        if(value <= 0){
+            System.out.println("Красный");
+        } else if (value >0 && value <=100){
+            System.out.println("Желтый");
+        } else {
+            System.out.printf("Зеленый");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 8;
+        int b = -16;
+
+        if(a>=b){
+            System.out.printf("a>=b");
+        } else {
+            System.out.printf("a<b");
+        }
+    }
+}
